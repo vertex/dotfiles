ad653fef9f1e28b1b6086ed790678cf3c4fec252
Bryon Keck <github@bryonkeck.com>
2024-06-17 16:45:26 -0700

    add nixos config

---
+++ nixos/common.nix
@@ -0,0 +1,30 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+
+  # Set your time zone.
+  time.timeZone = "America/Phoenix";
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+     wget
+     neovim
+     vim
+     git
+     lazygit
+     colima
+     lazydocker
+     steam
+  ];
+  programs.neovim.enable = true;
+  programs.neovim.defaultEditor = true;
+}
